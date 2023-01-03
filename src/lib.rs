78c2bfab7c50326b1743cf0b4a17c63bead3883d
hayas1 <h4ystack@gmail.com>
2023-01-03 09:21:22 +0000

    fixme

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,13 @@ where
     parenthetic::<N, _>(&mut target.chars().filter(|c| !c.is_whitespace()).peekable(), None)
 }
 
-fn parenthetic<N, E>(yet: &mut std::iter::Peekable<E>, p: Option<char>) -> anyhow::Result<N>
+fn parenthetic<N, E>(yet: &mut std::iter::Peekable<E>, open: Option<char>) -> anyhow::Result<N>
 where
     N: Clone + FromStr + Add<Output = N> + Sub<Output = N> + Mul<Output = N> + Div<Output = N>,
     <N as std::str::FromStr>::Err: 'static + std::marker::Sync + std::marker::Send + std::error::Error,
     E: Iterator<Item = char>,
 {
-    match (yet.peek(), p) {
+    match (yet.peek(), open) {
         // FIXME better match (peek and next is difficult)
         (Some(&paren), Some(pp)) if paren != pp => anyhow::bail!("expect {}, but found {}", pp, paren),
         (None, Some(pp)) => anyhow::bail!("expect {}, but found EOF", pp),
@@ -27,12 +27,12 @@ where
     };
 
     let closing_paren = |p| (p as u8 + 1) as char;
-    let qq = p.map(closing_paren);
+    let close = open.map(closing_paren);
 
     let result = expression(yet)?;
-
     match yet.next() {
-        q @ (None | Some(')' | '}' | ']')) if q == qq => Ok(result),
+        // FIXME: error message
+        q @ (None | Some(')' | '}' | ']')) if q == close => Ok(result),
         c => anyhow::bail!("expect end of expression, but found {:?}", c),
     }
 }
