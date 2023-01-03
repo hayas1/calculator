6e4798a65b8eab055ea265bc0c263b3ea2342971
hayas1 <h4ystack@gmail.com>
2023-01-03 06:33:33 +0000

    [wip] paren expression

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ where
     N: Clone + FromStr + Add<Output = N> + Sub<Output = N> + Mul<Output = N> + Div<Output = N>,
     <N as std::str::FromStr>::Err: 'static + std::marker::Sync + std::marker::Send + std::error::Error,
 {
-    Ok(expression::<N, _>(&mut target.chars(), None).unwrap()) // TODO
+    Ok(expression::<N, _>(&mut target.chars()).unwrap()) // TODO
 }
 
-fn expression<N, E>(yet: &mut E, paren: Option<char>) -> anyhow::Result<N>
+fn expression<N, E>(yet: &mut E) -> anyhow::Result<N>
 where
     N: Clone + FromStr + Add<Output = N> + Sub<Output = N> + Mul<Output = N> + Div<Output = N>,
     <N as std::str::FromStr>::Err: 'static + std::marker::Sync + std::marker::Send + std::error::Error,
@@ -21,7 +21,7 @@ where
     let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
     let t = term::<N, _>(yet);
     match yet.next() {
-        Some(op @ ('+' | '-')) => operate(t, op, expression(yet, paren)),
+        Some(op @ ('+' | '-')) => operate(t, op, expression(yet)),
         None => t,
         op => todo!("{:?}", op),
     }
@@ -45,11 +45,26 @@ where
     E: Iterator<Item = char> + itertools::PeekingNext,
 {
     let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
-    match yet.next().ok_or_else(|| anyhow::anyhow!("expect factor, but found EOF"))? {
-        p @ ('(' | '{' | '[') => expression(yet, Some((p as u8 + 1) as char)),
+    match yet.peeking_next(|_| true).ok_or_else(|| anyhow::anyhow!("expect factor, but found EOF"))? {
+        p @ ('(' | '{' | '[') => pexpression(yet, p),
         n if n.is_numeric() => constant(yet, n),
         c => Err(anyhow::anyhow!("expect factor, but found {}", c)),
     }
+}
+
+fn pexpression<N, E>(yet: &mut E, p: char) -> anyhow::Result<N>
+where
+    N: Clone + FromStr + Add<Output = N> + Sub<Output = N> + Mul<Output = N> + Div<Output = N>,
+    <N as std::str::FromStr>::Err: 'static + std::marker::Sync + std::marker::Send + std::error::Error,
+    E: Iterator<Item = char> + itertools::PeekingNext,
+{
+    let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
+    let result = expression(yet);
+    match yet.next() {
+        Some(q @ (')' | '}' | ']')) => anyhow::ensure!(q == (p as u8 + 1) as char, "not match paren"),
+        _ => todo!(),
+    }
+    result
 }
 
 fn constant<N, E>(yet: &mut E, n: char) -> anyhow::Result<N>
@@ -61,6 +76,7 @@ where
     let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
     let integer: String = yet.peeking_take_while(|&d| d.is_numeric()).collect();
     // TODO fraction
+    dbg!(n, &integer);
     Ok(N::from_str(&format!("{}{}", n, integer))?)
 }
 
