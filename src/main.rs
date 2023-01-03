ca52a6353d592f561546666e5f6f1b8faf4097c8
hayas1 <h4ystack@gmail.com>
2023-01-03 15:44:39 +0000

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
 use calculator::calculate;
 
-fn main() {
-    let args: Vec<_> = std::env::args().collect();
-    let expression = &args[1];
-    println!("{:?}", calculate::<i64>(expression))
+use clap::Parser;
+
+/// Simple program to calculate simple expression
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// expression to calculate
+    expression: String,
+
+    /// use rational
+    #[arg(short, long, default_value_t = false)]
+    precisely: bool,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    if args.precisely {
+        output_result(calculate::<num::Rational64>(&args.expression))
+    } else if args.expression.contains('.') {
+        output_result(calculate::<f64>(&args.expression))
+    } else {
+        output_result(calculate::<i64>(&args.expression))
+    }
+}
+
+fn output_result<T>(result: anyhow::Result<T>) -> Result<(), Box<dyn std::error::Error>>
+where
+    T: std::fmt::Display,
+{
+    match result {
+        Ok(n) => Ok(println!("{}", n)),
+        Err(e) => Err(e.into()),
+    }
 }
