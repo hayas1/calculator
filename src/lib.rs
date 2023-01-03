40a48408d7ade894b16d2ca5bdc131bfa2487364
hayas1 <h4ystack@gmail.com>
2023-01-03 09:11:19 +0000

    implement parenthetic

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,34 @@ where
     N: Clone + FromStr + Add<Output = N> + Sub<Output = N> + Mul<Output = N> + Div<Output = N>,
     <N as std::str::FromStr>::Err: 'static + std::marker::Sync + std::marker::Send + std::error::Error,
 {
-    Ok(expression::<N, _>(&mut target.chars().peekable()).unwrap())
+    parenthetic::<N, _>(&mut target.chars().peekable(), None)
+}
+
+fn parenthetic<N, E>(yet: &mut std::iter::Peekable<E>, p: Option<char>) -> anyhow::Result<N>
+where
+    N: Clone + FromStr + Add<Output = N> + Sub<Output = N> + Mul<Output = N> + Div<Output = N>,
+    <N as std::str::FromStr>::Err: 'static + std::marker::Sync + std::marker::Send + std::error::Error,
+    E: Iterator<Item = char> + itertools::PeekingNext,
+{
+    let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
+    match (yet.peek(), p) {
+        // FIXME better match (peek and next is difficult)
+        (Some(&paren), Some(pp)) if paren != pp => anyhow::bail!("expect {}, but found {}", pp, paren),
+        (None, Some(pp)) => anyhow::bail!("expect {}, but found EOF", pp),
+        (paren, pp) if paren == pp.as_ref() => yet.next(),
+        _ => None,
+    };
+
+    let closing_paren = |p| (p as u8 + 1) as char;
+    let qq = p.map(closing_paren);
+
+    let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
+    let result = expression(yet);
+
+    match yet.next() {
+        q @ (None | Some(')' | '}' | ']')) if q == qq => result,
+        c => anyhow::bail!("expect end of expression, but found {:?}", c),
+    }
 }
 
 fn expression<N, E>(yet: &mut std::iter::Peekable<E>) -> anyhow::Result<N>
@@ -19,11 +46,6 @@ where
     E: Iterator<Item = char> + itertools::PeekingNext,
 {
     let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
-    let closing_paren = |p| (p as u8 + 1) as char;
-    let q = if let Some('(' | '{' | '[') = yet.peek() { Some(closing_paren(yet.next().unwrap())) } else { None };
-    dbg!(q);
-
-    let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
     let mut result = term::<N, _>(yet);
 
     let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
@@ -31,10 +53,9 @@ where
         result = operate(result, yet.next().expect("peeked"), expression(yet));
     }
     match yet.peek() {
-        p @ (None | Some(')' | '}' | ']')) if p == q.as_ref() => yet.next(),
-        op => todo!("operator {:?}, q {:?}", op, q),
-    };
-    result
+        None | Some(')' | '}' | ']') => result,
+        c => anyhow::bail!("expect end of expression, but found {:?}", c),
+    }
 }
 
 fn term<N, E>(yet: &mut std::iter::Peekable<E>) -> anyhow::Result<N>
@@ -57,8 +78,8 @@ where
 {
     let _skip_whitespace = yet.peeking_take_while(|c| c.is_whitespace()).count();
     match yet.peek().ok_or_else(|| anyhow::anyhow!("expect factor, but found EOF"))? {
-        '(' | '{' | '[' => expression(yet),
-        &n if n.is_numeric() => constant(yet),
+        &p @ ('(' | '{' | '[') => parenthetic(yet, Some(p)),
+        n if n.is_numeric() => constant(yet),
         c => Err(anyhow::anyhow!("expect factor, but found {}", c)),
     }
 }
@@ -93,10 +114,21 @@ mod tests {
     #[test]
     fn test_add() {
         assert!(matches!(calculate::<i64>("1+2+3+4+5+6+7+8+9+10"), Ok(55)));
+        assert!(matches!(calculate::<i64>("1+2++3"), Err(_)));
+        assert!(matches!(calculate::<i64>("+2"), Err(_))); // TODO should be Ok?
     }
 
     #[test]
     fn test_paren() {
         assert!(matches!(calculate::<i64>("(1+2)+3"), Ok(6)));
+        assert!(matches!(calculate::<i64>("(1+2))+3"), Err(_)));
+    }
+
+    #[test]
+    fn test_whitespace() {
+        assert!(matches!(calculate::<i64>(" 1 +  2+3+4 +5+6+7+8+9+10  "), Ok(55)));
+        assert!(matches!(calculate::<i64>("1+( 2+3)"), Ok(6)));
+        assert!(matches!(calculate::<i64>("  1 + (2\t + 3\n\n  )"), Ok(6)));
+        assert!(matches!(calculate::<i64>("+2"), Err(_))); // TODO should be Ok?
     }
 }
